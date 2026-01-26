d31b0d27372460bc08897159b2fb115fbffdc84e
jRoblxz <joaoroblez76@gmail.com>
2026-01-26 15:59:14 -0300

    cors

--- TCC/config/cors.php
+++ TCC/config/cors.php
@@ -4,7 +4,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:5173', 'https://seusite.com'],
+    'allowed_origins' => ['http://localhost:3000', 'http://localhost:5173', 'https://projeto-tcc-jade.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
