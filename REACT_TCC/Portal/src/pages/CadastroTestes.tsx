d009e3d352a4c6f97ede436479f8a31809a2a922
jRoblxz <joaoroblez76@gmail.com>
2026-04-11 20:07:32 -0300

    a

--- REACT_TCC/Portal/src/pages/CadastroTestes.tsx
+++ REACT_TCC/Portal/src/pages/CadastroTestes.tsx
@@ -199,7 +199,7 @@ const CadastroTestes: React.FC = () => {
               </div>
             </div>
 
-            {/* Lado Direito: Foto */}
+            {/* Lado Direito: Foto asa*/}
             <div className="flex flex-col items-center justify-start">
               <label className="block text-sm font-bold text-gray-700 mb-2 text-center w-full">
                 Foto de Perfil
